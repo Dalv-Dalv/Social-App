659e4e5410653ad4b36524693d9c74780fc4fbb0
Dalv-Dalv <futurevlad2@gmail.com>
2025-11-18 15:33:23 +0200

    initial commit

---
+++ Social App/Data/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Social_App.Data {
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options) {
+    }
+}
---
+++ Social App/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Social_App.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
